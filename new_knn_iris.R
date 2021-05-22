246fdd12a90b23d510bb110a5c059fd3daaf8f10
drsharadbajpai <46110759+drsharadbajpai@users.noreply.github.com>
2021-05-22 15:33:31 -0400

    Normalizing the data for implementing knn

--- new_knn_iris.R
+++ new_knn_iris.R
@@ -17,3 +17,15 @@ normalize <- function(x) {
 }
 
 normalize(c(10,20,30,40,50))
+
+#Normalizing iris data
+iris_n <- as.data.frame(lapply(iris2[, c(1,2,3,4)], normalize))
+str(iris_n)
+summary(iris_n)
+
+#Creating Training and Testing datasets
+iris_train <- iris_n[1:129, ]
+iris_test <-iris_n[130:150, ]
+
+iris_train_target <- iris2[1:129, 5]
+iris_test_target <- iris2[130:150, 5]
