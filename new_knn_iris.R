2adaab469b02bbfbd211070f8364bebfbdca0ce2
drsharadbajpai <46110759+drsharadbajpai@users.noreply.github.com>
2021-05-22 14:35:19 -0400

    Created Normalizing formula

--- new_knn_iris.R
+++ new_knn_iris.R
@@ -8,5 +8,12 @@ gp <- runif(nrow(iris))
 iris2 <- iris[order(gp),]
 str(iris2)             
 head(iris2)
-
 summary(iris2[, c(1,2,3,4)])
+
+#Normalization of the data
+
+normalize <- function(x) {
+  return( (x - min(x)) / (max(x) - min(x)))
+}
+
+normalize(c(10,20,30,40,50))
