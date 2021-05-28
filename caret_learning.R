3b168ecb689086f415f15de6d991b02a079f2956
drsharadbajpai <46110759+drsharadbajpai@users.noreply.github.com>
2021-05-28 19:45:07 -0400

    Creating gbm model and tuning it

--- caret_learning.R
+++ caret_learning.R
@@ -2,6 +2,7 @@
 library(C50)
 library(caret)
 library(readr)
+library(modeldata)
 
 data("mlc_churn")
 churn <- mlc_churn
@@ -10,8 +11,43 @@ str(churn)
 
 predictors <- names(churn)[names(churn)!= "churn"] 
 data_df <- as.data.frame(churn)
-set.seed(1)
-inTrainingSet<- createDataPartition(data_df$Churn, p =.75, list= FALSE)
+
+#Data Splitting Step
+set.seed(7)
+inTrainingSet<-caret::createDataPartition(data_df$churn, p = .75, list= FALSE)
 churnTrain <- data_df[inTrainingSet,]
 churnTest <- data_df[-inTrainingSet,]
+
+#Data Pre-processing Methods
+numerics <- c("account_length", "total_day_calls", "total_night_calls")
+
+#Determine means and sd's
+proc_Values <- preProcess(churnTrain[, numerics], method = c("center","scale","YeoJohnson"))
+
+#Use the predict methods to do the adjustments
+trainScaled <- predict(proc_Values,churnTrain[, numerics])
+testScaled <- predict(proc_Values, churnTest[, numerics])
+
+proc_Values
+
+#Boosted Trees Parameters:
+#Using gbm library
+
+library(gbm)
+forGBM <- churnTrain
+forGBM$churn <- ifelse(forGBM$churn == "yes", 1 , 0)
+
+gbmFit <- gbm(formula = churn~.,           #Use all predictors
+              distribution = "bernoulli",    #For classification
+              data = forGBM,
+              n.trees = 2000,                #2000 boosting iterations
+              interaction.dept = 7,          #How many splits in each tree
+              shrinkage = 0.01,              #learning rate
+              verbose = FALSE                #Do not print the details
+)
+
+#Model Tuning using the "train"
+library(e1071)
+gbmTune <- train(churn ~. ,data = churnTrain, method = "gbm", verbose= FALSE)
+
 
